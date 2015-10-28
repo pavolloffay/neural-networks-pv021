a1af11213100b1c93e8ff500e7e26bb69b737801
Pavol Loffay <p.loffay@gmail.com>
2015-10-28 21:11:04 +0100

    hello world

---
+++ src/main/java/cz/muni/neural/network/forecast/App.java
@@ -0,0 +1,11 @@
+package cz.muni.neural.network.forecast;
+
+/**
+ * @author Pavol Loffay
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
